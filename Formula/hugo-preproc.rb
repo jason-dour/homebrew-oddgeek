0fea09ad6ecb69a06c3372c1b10cdb4ec77bba03
Jason A. Dour <10261648+jason-dour@users.noreply.github.com>
2026-07-07 22:19:00 -0400

    Update HugoPreproc formula to use git URL and remove go-task dependency

--- Formula/hugo-preproc.rb
+++ Formula/hugo-preproc.rb
@@ -2,13 +2,12 @@ class HugoPreproc < Formula
     desc "Helper for Hugo to provide for pre-processing of files."
     homepage "https://github.com/jason-dour/hugo-preproc"
     version "2.1.1"
-    url "https://github.com/jason-dour/hugo-preproc/archive/refs/tags/v2.1.1.tar.gz"
+    url "https://github.com/jason-dour/hugo-preproc.git", tag: "v2.1.1", revision: "c6042c99a0f601337fa18cc7a2d4ac5123b0371f"
     sha256 "f4da8c118bb84f0798e4572b8e2711a8931b9e37529a6e2474f964ef6bb144ae"
     license "AGPL-3.0-or-later"
     head "https://github.com/jason-dour/hugo-preproc.git", branch: "main"
 
     depends_on "go"
-    depends_on "go-task"
 
     def install
         system "go", "build", *std_go_args(ldflags: "-X {{.MODULE}}/internal/cmn.Version=#{version} -X {{.MODULE}}/internal/cmn.Basename=#{File.basename(buildpath)} -X {{.MODULE}}/internal/cmn.Commit=#{Utils.git_head}"), "./cmd/hugo-preproc"
